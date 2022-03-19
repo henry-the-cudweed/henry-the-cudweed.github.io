e64dc8d384e7df10a2de0080f49db611bde7526b
henry-the-cudweed <88332473+henry-the-cudweed@users.noreply.github.com>
2022-03-19 14:33:33 -0700

    updated shmindex to only include recent workday polygons

---
+++ workshapes.js
@@ -0,0 +1,10 @@
+var capeIvyShape = { "type": "FeatureCollection", "features": [
+    { "type": "Feature", "properties": 
+    { "Slider": "slider-mg108283.html", "Location Description":"South side of volunteer canyon stream, west of lateral Bay Laurel til two trunks on bank","Common Name":"Cape ivy","Taxon":"Delairea odorata","Observer":"Henry Inman","Access":"published","ID":"mg108283","Date":"2020-03-18 17:43:41.0"}, "geometry": {"coordinates":[[[-122.6724551,37.9265451],[-122.6724624,37.926573],[-122.6725117,37.9266443],[-122.6724369451,37.9266726909],[-122.6723806188,37.9266996664],[-122.6723069,37.9266928],[-122.6724551,37.9265451]]],"type":"Polygon"}},
+
+    { "type": "Feature", "properties": 
+    { "Slider": "mg155769-slider.html", "Location Description":"South side of volunteer canyon Creek, from the two willows on the bank to the arching bay tree","Common Name":"Cape ivy","Taxon":"Delairea odorata","Observer":"Henry Inman","Access":"unpublished","ID":"mg155769","Date":"2022-02-09 12:34:32.0"}, "geometry": {"coordinates":[[[-122.6726599,37.9264891],[-122.6726112,37.9264852],[-122.6725809,37.9265296],[-122.6725778,37.9265544],[-122.6725393,37.9265349],[-122.6725174,37.9265094],[-122.6725652,37.9264779],[-122.6726359,37.9264118],[-122.6726643,37.9264333],[-122.6726599,37.9264891]]],"type":"Polygon"}},
+
+    
+
+]}
