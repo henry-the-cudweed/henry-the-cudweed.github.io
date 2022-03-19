f6a252e07644c48c460dded1c2dc26a656a66ba4
henry-the-cudweed <88332473+henry-the-cudweed@users.noreply.github.com>
2022-03-19 14:37:49 -0700

    updating mg155769

--- mg155769-geojson.js
+++ mg155769-geojson.js
@@ -1,5 +1,9 @@
-var capeIvyShape = 
-{ "type": "FeatureCollection", "features": [
-    { "type": "Feature", "properties": 
-    {"Location Description":"South side of volunteer canyon Creek, from the two willows on the bank to the arching bay tree","Common Name":"Cape ivy","Taxon":"Delairea odorata","Observer":"Henry Inman","Access":"unpublished","ID":"mg155769","Date":"2022-02-09 12:34:32.0"}, "geometry": {"coordinates":[[[-122.6726599,37.9264891],[-122.6726112,37.9264852],[-122.6725809,37.9265296],[-122.6725778,37.9265544],[-122.6725393,37.9265349],[-122.6725174,37.9265094],[-122.6725652,37.9264779],[-122.6726359,37.9264118],[-122.6726643,37.9264333],[-122.6726599,37.9264891]]],"type":"Polygon"}}
-    ]}
+var capeIvyShape =
+{
+    "type": "FeatureCollection", "features": [
+        {
+            "type": "Feature", "properties":
+                { "Location Description": "South side of volunteer canyon Creek, from the two willows on the bank to the arching bay tree", "Common Name": "Cape ivy", "Taxon": "Delairea odorata", "Observer": "Henry Inman", "Access": "unpublished", "ID": "mg155769", "Date": "2022-02-09 12:34:32.0" }, "geometry": { "coordinates": [[[-122.6726599, 37.9264891], [-122.6726112, 37.9264852], [-122.6725809, 37.9265296], [-122.6725778, 37.9265544], [-122.6725393, 37.9265349], [-122.6725174, 37.9265094], [-122.6725652, 37.9264779], [-122.6726359, 37.9264118], [-122.6726643, 37.9264333], [-122.6726599, 37.9264891]]], "type": "Polygon" }
+        }
+    ]
+}
