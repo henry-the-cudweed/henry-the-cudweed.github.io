530dcd3a1828a25dffe0f17604aa5eeb963b1e1b
henry-the-cudweed <88332473+henry-the-cudweed@users.noreply.github.com>
2022-03-19 15:35:56 -0700

    march map

--- workshapes.js
+++ workshapes.js
@@ -1,10 +1,9 @@
 var capeIvyShape = { "type": "FeatureCollection", "features": [
-    { "type": "Feature", "properties": 
-    { "Slider": "slider-mg108283.html", "Location Description":"South side of volunteer canyon stream, west of lateral Bay Laurel til two trunks on bank","Common Name":"Cape ivy","Taxon":"Delairea odorata","Observer":"Henry Inman","Access":"published","ID":"mg108283","Date":"2020-03-18 17:43:41.0"}, "geometry": {"coordinates":[[[-122.6724551,37.9265451],[-122.6724624,37.926573],[-122.6725117,37.9266443],[-122.6724369451,37.9266726909],[-122.6723806188,37.9266996664],[-122.6723069,37.9266928],[-122.6724551,37.9265451]]],"type":"Polygon"}},
-
+    
     { "type": "Feature", "properties": 
     { "Slider": "mg155769-slider.html", "Location Description":"South side of volunteer canyon Creek, from the two willows on the bank to the arching bay tree","Common Name":"Cape ivy","Taxon":"Delairea odorata","Observer":"Henry Inman","Access":"unpublished","ID":"mg155769","Date":"2022-02-09 12:34:32.0"}, "geometry": {"coordinates":[[[-122.6726599,37.9264891],[-122.6726112,37.9264852],[-122.6725809,37.9265296],[-122.6725778,37.9265544],[-122.6725393,37.9265349],[-122.6725174,37.9265094],[-122.6725652,37.9264779],[-122.6726359,37.9264118],[-122.6726643,37.9264333],[-122.6726599,37.9264891]]],"type":"Polygon"}},
 
-    
+    { "type": "Feature", "properties": 
+{ "Slider": "mg155840-slider.html", "Infested Area (acres)":"210.1 Square Meters","Number of Plants":"","Date / Time":"2022-02-11 12:22:52.0","Location Description":"","Common Name":"Cape ivy","Percent Cover":"50 - 75 %","Taxon":"Delairea odorata","Gross Area (acres)":"336.9 Square Meters","Phenology":"","Observer":"Henry Inman","Distribution":"","Habitat":"","ID":"mg155840","Notes":""}, "geometry": {"coordinates":[[[-122.6725667,37.9267888],[-122.6726643,37.9268257],[-122.6727055,37.9267358],[-122.6727293,37.9266601],[-122.6727164,37.9265679],[-122.6727128,37.9265233],[-122.6726556,37.9265162],[-122.6726279,37.926532],[-122.6726203,37.9265638],[-122.6726161,37.9266059],[-122.6725574,37.9266305],[-122.6725958,37.9267123],[-122.6725667,37.9267888]]],"type":"Polygon"}}
 
 ]}
